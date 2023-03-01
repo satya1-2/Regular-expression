ac142c482e583de3161c66fac591f43ff9270f63
satya <satya143@gmail.com>
2023-03-01 14:25:16 +0530

    Added:Check valid last name with mimimum 3 chracter

--- src/regularExpression/ValidUserLastNameCheck.java
+++ src/regularExpression/ValidUserLastNameCheck.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ValidUserNameCheck {
+public class ValidUserLastNameCheck {
     public void checkRegularExpPattern() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name: ");
-        String name = sc.nextLine();
+        System.out.println("Enter your Last name: ");
+        String lastName = sc.nextLine();
         Pattern p = Pattern.compile("[a-zA-Z]{3,}");
-        Matcher m = p.matcher(name);
+        Matcher m = p.matcher(lastName);
         boolean b = m.matches();
         System.out.println(b);
     }
 
     public static void main(String[] args) {
-        ValidUserNameCheck search = new ValidUserNameCheck();
+        ValidUserLastNameCheck search = new ValidUserLastNameCheck();
         search.checkRegularExpPattern();
     }
 }
